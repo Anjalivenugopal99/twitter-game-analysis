bced434df2be09563ed4c40478e0929e2a3f02c7
Visakh Sakthidharan <anjali94venu@gmail.com>
2021-02-10 11:41:37 -0600

    initial commit

---
+++ py/data_mining/dump.py
@@ -0,0 +1,12 @@
+  
+from . import settings
+import tweepy
+import dataset
+from textblob import TextBlob
+from datafreeze import freeze
+
+db = dataset.connect(settings.CONNECTION_STRING)
+
+result = db[settings.TABLE_NAME].all()
+freeze(result, format='csv', filename=settings.CSV_NAME)# -*- coding: utf-8 -*-
+
---
+++ py/data_mining/settings.py
@@ -0,0 +1,11 @@
+  
+TRACK_TERMS = ["SuperBowl", "SuperBowl2021","RunItback","KC","SuperBowlLV","SBLV","buccaneers"]
+CONNECTION_STRING  = "sqlite:///tweets.db"
+CSV_NAME = "tweets_superbowl.csv"
+TABLE_NAME = "Twitter_data"
+consumer_key = 'FAep2spJzYlQ7aoDfxofrytrw'
+consumey_secret = '3u0CAWqrEzAd9cWo8KmDKOaxg1edzudKzts0P1tZTb2utIruj7'
+acess_token = '1349047995984187393-ZH4ZL2qqoAWKp4GxWQs8XJpucVmEGA'
+acess_secret = '1PZkn6kCv6nICjkhbcrbUG7sVvnRSIaOQF4lS07bJG8uS'
+
+
